2c73cb6f684a2026d34b0438c1718d448553f951
Harshit Kumar <harshitk11@gmail.com>
2022-10-23 00:34:05 -0400

    cleaned version

--- src/parse_logcat.py
+++ src/parse_logcat.py
@@ -1,20 +1,29 @@
-# Script to parse the logcat file to extract the following information from it:
-# 1) Number of lines 
-# 2) Timestamp difference to see how long the application executes
-# 3) Rate at which logcat events are happening
-
+"""
+Script to parse the logcat file to extract the following information from it:
+1) Number of lines 
+2) Timestamp difference to see how long the application executes
+3) Rate at which logcat events are happening
+"""
 import re
 from datetime import datetime
 from datetime import timedelta
 import statistics
 
-# Class containg all the methods that aid in parsing the logcat file
 class logcat_parser:
-    
-    # Function to read a logcat file and extract the list of timestamps
-    # Argument : filename (specify with file path)
+    """
+    Class containing all the methods that aid in parsing the logcat file.
+    """
     @staticmethod
     def extract_events(logcat_filename):
+        """
+        Function to read a logcat file and extract the list of timestamps
+        
+        params:
+            - logcat_filename: specify with file path
+
+        Output:
+            - timestamp_list: list of timestamp of the events
+        """
         # List to store the extracted time stamps
         timestamp_list = []
         rfile = open(logcat_filename, 'r', errors='ignore') # Ignore the encoding errors. Ref: 'https://docs.python.org/3/library/functions.html#open'
@@ -43,18 +52,21 @@ class logcat_parser:
         rfile.close()
         # Return list of timestamp of the events
         return timestamp_list
-    #-------------------------------------------------------------------------------------------------------------------#
-    # Function to read the timestamp list and get the timestamp difference between the last and the first event
-    # Argument : List of timestamps
+    
     @staticmethod
     def get_time_difference(tstamp_list):
+        """
+        Function to read the timestamp list and get the timestamp difference between the last and the first event
+        params:
+            - tstamp_list: List of timestamps
+        Output:
+            - timestamp difference
+        """
         if (len(tstamp_list) > 0): # Need to have atleast one event in logcat to get the time difference
             start_time = tstamp_list[0]
             end_time = tstamp_list[-1]
             time_format = '%H:%M:%S.%f'
             
-            # print(datetime.strptime(end_time, time_format))
-            # print(datetime.strptime(start_time, time_format))
             t_delta = datetime.strptime(end_time, time_format) - datetime.strptime(start_time, time_format)
 
             # Corner case where interval might cross midnight
@@ -70,19 +82,24 @@ class logcat_parser:
         else:
             return 0
 
-    #-------------------------------------------------------------------------------------------------------------------#
-    # Function to return the number of lines in the timestamp list
-    # Argument : List of timestamps
     @staticmethod
     def get_logcat_lines(tstamp_list):
+        """ 
+        Function to return the number of lines in the timestamp list
+        params: 
+            - tstamp_list: List of timestamps
+        """
         return len(tstamp_list)
 
-    #-------------------------------------------------------------------------------------------------------------------#
-    # Function to calculate the average frequency of events using the timestamp list
-    # Argument : List of timestamps
     @staticmethod
     def get_average_frequency(tstamp_list):
-        if (len(tstamp_list) > 1): # You need to have atleast 2 timestamps in the list to get the time difference
+        """
+        Function to calculate the average frequency of events using the timestamp list
+        params: 
+            - tstamp_list: List of timestamps
+        """
+        # You need to have atleast 2 timestamps in the list to get the time difference
+        if (len(tstamp_list) > 1): 
             time_format = '%H:%M:%S.%f'
             
             # Calculate the time difference between successive events and store the difference in a list
@@ -101,15 +118,15 @@ class logcat_parser:
             avg_freq = 0
 
         return avg_freq
-    #-------------------------------------------------------------------------------------------------------------------#    
-
 
 
 def main():
-    # print(logcat_parser.extract_time_difference(logcat_parser.extract_events('/data/hkumar64/projects/arm-telemetry/codes/_com_dealmoon_android_logcat_iter_0_rn3.txt')))
-    print(logcat_parser.get_average_frequency(logcat_parser.extract_events('/data/hkumar64/projects/arm-telemetry/logcat_files/results_android_zoo_malware_all_rerun/com_taomi_beauty_130AD68D523D08F47F2A776CFFBE62E4645AD3E6629FF741654DCFC8361A6F1A.apk/logcat/_com_taomi_beauty_logcat_iter_0_rn1.txt')))
-    # print(logcat_parser.get_logcat_lines(logcat_parser.extract_events('/data/hkumar64/projects/arm-telemetry/codes/_com_dealmoon_android_logcat_iter_0_rn3.txt')))
-    
+    ############ Testing ############
+    # path = '/data/hkumar64/projects/arm-telemetry/logcat_files/results_android_zoo_malware_all_rerun/com_taomi_beauty_130AD68D523D08F47F2A776CFFBE62E4645AD3E6629FF741654DCFC8361A6F1A.apk/logcat/_com_taomi_beauty_logcat_iter_0_rn1.txt'
+    # print(logcat_parser.get_average_frequency(logcat_parser.extract_events(path)))
+    # print(logcat_parser.get_logcat_lines(logcat_parser.extract_events(path)))
+    # print(logcat_parser.extract_time_difference(logcat_parser.extract_events(path)))
+    #################################
 if(__name__=="__main__"):
     main()
 
